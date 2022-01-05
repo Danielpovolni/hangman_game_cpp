a361d6b488b958bf5d68c36855b687d6350aabe9
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2022-01-05 22:55:16 +0100

    Create code.cpp
    test

---
+++ code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word;
+    cin >> word;
+    //                    0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
+    string alphabet[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    int length_of_word = word.length();
+
+    char str[1000];
+    int l=0;
+    cout << word[1];
+
+    for(int i=0; word[i] != '\0fi'; ++i)
+    {
+        cout << word[i] << endl;
+        l++;
+    }
+
+
+
+
+
+
+
+
+
+}
