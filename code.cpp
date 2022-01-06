bc161331bba842546554bfcd12e0604bd235e18a
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2022-01-07 00:10:13 +0100

    Update code.cpp
    algorithm that actually works for gods sake

--- code.cpp
+++ code.cpp
@@ -11,13 +11,16 @@ int main(){
     //                  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
     char alphabet[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int length_of_word = word.length();
-    while(bool1 == false)
+    while(bool1 == false){
         cin >> guest;
-        for(int i=0; i < 5;i++){
+        for(int i=0; i < length_of_word;i++){
             for(int x=0; x<26;x++){
                 if(word[i]==alphabet[x]){
-                    cout << i;
-                }}}
+                    cout << x << endl;
+                    bool1 = true;
+                }}
+                }}
+
 
 
 }
