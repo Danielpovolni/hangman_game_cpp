f0123729eaa40f154ea01b6980d83b7b7e39f32f
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2022-01-07 00:22:40 +0100

    Create find_letters_in_alphabet.cpp

---
+++ find_letters_in_alphabet.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word;
+    cin >> word;
+    string guest;
+
+    bool bool1 = false;
+    //                  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
+    char alphabet[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int length_of_word = word.length();
+    while(bool1 == false){
+        cin >> guest;
+        for(int i=0; i < length_of_word;i++){
+            for(int x=0; x<26;x++){
+                if(word[i]==alphabet[x]){
+                    cout << x << endl;
+                    bool1 = true;
+                }}
+                }}
+
+
+
+}
