2c1bf639c378e4f5322c8cb3da94ee6d2dc6df63
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2022-01-07 17:23:51 +0100

    Update code.cpp
    working demo

--- code.cpp
+++ code.cpp
@@ -2,25 +2,26 @@
 #include <string>
 using namespace std;
 
+
 int main(){
     string word;
     cin >> word;
-    string guest;
+    char guest;
 
     bool bool1 = false;
     //                  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
     char alphabet[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char alphabet2[] = {'_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'};
     int length_of_word = word.length();
     while(bool1 == false){
         cin >> guest;
-        for(int i=0; i < length_of_word;i++){
-            for(int x=0; x<26;x++){
-                if(word[i]==alphabet[x]){
-                    cout << x << endl;
-                    bool1 = true;
-                }}
-                }}
+        for(int i =0;i < length_of_word;i++){
+            if(guest==word[i]){
+                alphabet2[i]=guest;}}
 
+        for(int y=0;y<length_of_word;y++){
+        cout << alphabet2[y];}
+    }
 
+    }
 
-}
