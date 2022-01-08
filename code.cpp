f6e9467a60a9e62fa7afa57502e4fb88237b9ed1
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2022-01-08 19:07:12 +0100

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -7,7 +7,7 @@ int main(){
     string word;
     cout << "Enter word you want to guest: ";
     cin >> word;
-    char guest;
+    char guess;
 
     bool bool1 = false;
     //                  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
@@ -16,12 +16,20 @@ int main(){
     int length_of_word = word.length();
     while(bool1 == false){
         cout << "Enter letter you want to quest: ";
-        cin >> guest;
+        cin >> guess;
         for(int i =0;i < length_of_word;i++){
-            if(guest==word[i]){
-                alphabet2[i]=guest;}
-            cout << alphabet2[i];
+            // find guess in word    
+            if(guess==word[i]){
+                alphabet2[i]=guess;}
             }
-            cout << endl;
+            //check if guess is in the word
+            if(word.find(guess) == string::npos){
+                cout << "NO LATTER IN WORD" << endl;
+            }
+        // print word with latter that have been found
+        for(int x=0;x < length_of_word; x++){
+            cout << alphabet2[x];
+        }
+        cout << endl;
     }
     }
